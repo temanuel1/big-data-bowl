df2d8cf8a28fc22339aef37463bd59a5a2e9f3dd
Tyler Emanuel <tyleremanuel@Tylers-MacBook-Pro-3.local>
2023-10-25 17:18:12 -0500

    load rest of data

--- Test.R
+++ Test.R
@@ -1,2 +1,4 @@
-games = read.csv("games.csv")
-games
+games = read.csv("data/games.csv")
+players = read.csv("data/players.csv")
+plays = read.csv("data/plays.csv")
+tackles = read.csv("data/tackles.csv")
